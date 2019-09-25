369799e12b1f9410594d19434536455b4acb70df
Drache Havoc <drache.havoc@gmail.com>
2019-09-25 12:17:40 -0300

    hrs minutos

--- src/public/cadastro/main.js
+++ src/public/cadastro/main.js
@@ -331,8 +331,13 @@ document
             divDateActivitiesType = divDateActivities.querySelector('div.activity-type:last-child')
         }
 
-        let min = (acitivity.duration - Math.floor(acitivity.duration)) * 60;
-        let hrs = acitivity.duration;
+        let min = acitivity.duration - Math.floor(acitivity.duration);
+        let hrs = acitivity.duration - min;
+
+        min = min * 60;
+
+        if (hrs < 10) hrs = "0" + hrs;
+        if (min < 10) min = "0" + min;
 
         divDateActivitiesType.innerHTML += `
             <div class="activity" data-id="${acitivity.id}">
